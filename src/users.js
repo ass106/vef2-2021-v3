c1864849802ec58ebf669d446d9cfcacc793aac1
ass106 <ass@hi.is>
2021-02-28 21:50:22 +0000

    pg-format

--- src/users.js
+++ src/users.js
@@ -1,53 +1,25 @@
 import bcrypt from 'bcrypt';
+import { query } from './db.js';
 
-const records = [
-  {
-    id: 1,
-    username: 'admin',
-
-    // 123
-    password: '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii',
-    admin: true,
-  },
-  {
-    id: 2,
-    username: 'stradi',
-
-    // 123
-    password: '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii',
-    admin: false,
-  },
-];
-
-export async function comparePasswords(password, user) {
-  const ok = await bcrypt.compare(password, user.password);
-
-  if (ok) {
-    return user;
-  }
-
-  return false;
-}
-
-// Merkjum sem async þó ekki verið að nota await, þar sem þetta er notað í
-// app.js gerir ráð fyrir async falli
 export async function findByUsername(username) {
-  const found = records.find((u) => u.username === username);
+  const q = 'SELECT * FROM users WHERE username = $1';
 
-  if (found) {
-    return found;
+  const result = await query(q, [username]);
+
+  if (result.rowCount === 1) {
+    return result.rows[0];
   }
 
   return null;
 }
 
-// Merkjum sem async þó ekki verið að nota await, þar sem þetta er notað í
-// app.js gerir ráð fyrir async falli
 export async function findById(id) {
-  const found = records.find((u) => u.id === id);
+  const q = 'SELECT * FROM users WHERE id = $1';
 
-  if (found) {
-    return found;
+  const result = await query(q, [id]);
+
+  if (result.rowCount === 1) {
+    return result.rows[0];
   }
 
   return null;
